3942466b38b47cfc6e1233ee71b90c216dd75fd0
Annesa Mariyam <annesa-mariyam@hotmail.co.uk>
2021-08-03 15:07:09 +0100

    cat image

--- app.rb
+++ app.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require "sinatra/reloader" if development?
 
 get '/secret' do
   "Sssshhh this is a secret"
+end
+
+get '/cat' do
+  '<img style ="border:dashed red;" src="https://i.imgur.com/jFaSxym.png">'
+  # send_file "cat.html"
 end
